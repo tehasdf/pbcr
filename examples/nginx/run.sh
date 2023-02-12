0719674bcd70ef3fabceecb68289c1ff97390e65
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2023-02-12 23:36:38 +0100

    add a nginx example with a copy

---
+++ examples/nginx/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eux
+
+dir=$(dirname "${BASH_SOURCE[0]}")
+
+pbcr run \
+    docker.io/library/nginx:sha256:7f797701ded5055676d656f11071f84e2888548a2e7ed12a4977c28ef6114b17 \
+    -n c1 \
+    --copy "${dir}/default.conf:/etc/nginx/conf.d/default.conf"

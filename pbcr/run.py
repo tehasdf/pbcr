6f75e5dedbcb75494b3da22843bde5dc1cd4ef1d
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2023-02-14 22:08:00 +0100

    refactor run.py a bit
    move things to separate objects, to satisfy pylint's ideas of
    allowed amount of local variables

--- pbcr/run.py
+++ pbcr/run.py
@@ -1,14 +1,18 @@
+"""Commands and utilities for running a container"""
+
 import ctypes
 import json
 import os
-import sys
-import uuid
-import subprocess
+import pathlib
 import signal
+import subprocess
+import sys
 import threading
+import uuid
+from typing import Iterable
 
 from pbcr.docker_registry import load_docker_image
-from pbcr.types import Storage, Container
+from pbcr.types import Storage, Container, Image
 
 libc = ctypes.CDLL('libc.so.6')
 
@@ -19,120 +23,74 @@ CLONE_NEWUSER = 0x10000000
 CLONE_NEWCGROUP = 0x02000000
 
 
-def _get_container_spec(mnt_cmd, container_dir):
-    container_chroot = container_dir / 'chroot'
-    parentpid = os.getpid()
-    evt = threading.Event()
-    signal.signal(signal.SIGUSR1, lambda sig, frame: evt.set())
+class _UIDMapper:
+    def __init__(self, subuid: str, subgid: str, root_map: str):
+        self._subuid = subuid
+        self._subgid = subgid
+        self._root_map = root_map
 
-    newuidmap_args = ['0', str(os.getuid()), '1']
-    newgidmap_args = ['0', str(os.getgid()), '1']
+    def _format_args(self, ids: set[str], start: str) -> list[str]:
+        args = ['0', self._root_map, '1']
 
-    forkpid = os.fork()
-    if forkpid == 0:
-        libc.unshare(CLONE_NEWUSER | CLONE_NEWCGROUP | CLONE_NEWNS)
-        os.kill(parentpid, signal.SIGUSR1)
-        evt.wait()
-        evt.clear()
+        # root, not part of this map, we'll always add it to the args anyway
+        ids = ids - {'0'}
+        if ids:
+            min_id = min(int(u) for u in ids)
+            max_id = max(int(u) for u in ids)
+            args += [str(min_id), start, str(max_id - min_id + 1)]
+
+        return args
+
+    def newuidmap(self, pid: int, uids: Iterable[str]):
+        """Use newuidmap to subuid the given ids, for the given pid"""
+        args = self._format_args(set(uids), self._subuid)
+        subprocess.call(['/usr/bin/newuidmap', str(pid)] + args)
+
+    def newgidmap(self, pid: int, gids: Iterable[str]):
+        """Use newgidmap to subgid the given ids, for the given pid"""
+        args = self._format_args(set(gids), self._subgid)
+        subprocess.call(['/usr/bin/newgidmap', str(pid)] + args)
+
+
+class _ContainerFS:
+    @classmethod
+    def prepare(cls, container_dir: pathlib.Path, lowers: list[pathlib.Path]):
+        """Create the directory structure at container_dir"""
+        (container_dir / 'upper').mkdir(exist_ok=True)
+        (container_dir / 'workdir').mkdir(exist_ok=True)
+        (container_dir / 'chroot').mkdir(exist_ok=True)
+        return cls(container_dir, lowers)
+
+    def __init__(
+        self,
+        container_dir: pathlib.Path,
+        lowers: list[pathlib.Path]
+    ):
+        self.container_chroot = container_dir / 'chroot'
+        self.container_dir = container_dir
+        self._lowers = lowers
+        self._container_workdir = container_dir / 'workdir'
+        self._container_upperdir = container_dir / 'upper'
+
+    def mount(self):
+        """Mount the overlayfs at the container chroot"""
+        lowers = ':'.join(reversed([str(lower) for lower in self._lowers]))
+        upper = self._container_upperdir
+        workdir = self._container_workdir
+        mnt_cmd = [
+            '/bin/mount', '-t', 'overlay', 'overlay',
+            '-o', f'lowerdir={lowers},upperdir={upper},workdir={workdir}',
+            f'{self.container_chroot}'
+        ]
         subprocess.call(mnt_cmd)
 
-        container = {}
-        try:
-            uids = []
-            with (container_chroot / 'etc' / 'passwd').open() as f:
-                for line in f:
-                    parts = line.split(':')
-                    uid = parts[2]
-                    uids.append(uid)
-            container['uids'] = uids
-        except IOError:
-            container['uids'] = []
-
-        try:
-            gids = []
-            with (container_chroot / 'etc' / 'group').open() as f:
-                for line in f:
-                    parts = line.split(':')
-                    gid = parts[2]
-                    gids.append(gid)
-            container['gids'] = gids
-        except IOError:
-            container['gids'] = []
-
-        with (container_dir / 'container.json').open('w') as f:
-            json.dump(container, f)
-
-        os.kill(parentpid, signal.SIGUSR1)
-        sys.exit(0)
-
-    else:
-        evt.wait()
-        evt.clear()
-
-        subprocess.call(['/usr/bin/newuidmap', str(forkpid)] + newuidmap_args)
-        subprocess.call(['/usr/bin/newgidmap', str(forkpid)] + newgidmap_args)
-
-        os.kill(forkpid, signal.SIGUSR1)
-        evt.wait()
-        evt.clear()
-        with (container_dir / 'container.json').open() as f:
-            container_data = json.load(f)
-
-
-        if set(container_data['uids']) > {'0'}:
-            min_uid = min(int(u) for u in container_data['uids'] if u != '0')
-            max_uid = max(int(u) for u in container_data['uids'] if u != '0')
-            newuidmap_args += [
-                str(min_uid), '100000', str(max_uid - min_uid + 1)]
-
-        if set(container_data['gids']) > {'0'}:
-            min_gid = min(int(u) for u in container_data['gids'] if u != '0')
-            max_gid = max(int(u) for u in container_data['gids'] if u != '0')
-            newgidmap_args += [
-                str(min_gid), '100000', str(max_gid - min_gid + 1)]
-
-        signal.signal(signal.SIGUSR1, signal.SIG_DFL)
-        return newuidmap_args, newgidmap_args
-
-
-def run_command(
-    storage: Storage,
-    image_name: str,
-    container_name: str | None=None,
-    daemon: bool=False,
-    volumes: list[str] | None=None,
-):
-    if not container_name:
-        container_name = str(uuid.uuid4())
-    if image_name.startswith('docker.io/'):
-        image_name = image_name.replace('docker.io/', '', 1)
-        img = load_docker_image(storage, image_name)
-    else:
-        raise ValueError(f'unknown image reference: {image_name}')
-
-    container = Container(
-        id=container_name,
-        image_registry=img.registry,
-        image_name=img.manifest.name,
-        pid=None,
-    )
-    storage.store_container(container)
-    container_dir = storage.make_container_dir(container_name, img)
-
-    entrypoint = img.config.config['Entrypoint']
-    command = img.config.config['Cmd']
-    container_chroot = container_dir / 'chroot'
-    container_workdir = container_dir / 'workdir'
-    container_upperdir = container_dir / 'upper'
-    container_chroot.mkdir(exist_ok=True)
-    container_upperdir.mkdir(exist_ok=True)
-    container_workdir.mkdir(exist_ok=True)
-
-    lowers =  ':'.join(str(ll.path) for ll in reversed(img.layers))
-
-    if volumes:
-        container_volumes = container_dir / 'volumes'
+    def add_volumes(self, volumes: list[str]):
+        """Add volumes to this container's "lower" overlayfs dirs"""
+        if not volumes:
+            return
+        container_volumes = self.container_dir / 'volumes'
         container_volumes.mkdir(exist_ok=True)
+
         for volume in volumes:
             volume_source, _, volume_target = volume.partition(':')
             volume_target = volume_target.lstrip('/')
@@ -142,54 +100,162 @@ def run_command(
             if target.exists():
                 os.unlink(target)
             os.link(volume_source, target)
-        lowers = f'{container_volumes}:{lowers}'
+            self._lowers.append(container_volumes)
+
+    def chroot(self):
+        """Chroot into this container's chroot"""
+        os.chroot(self.container_chroot)
+        os.chdir('/')
+
+    def remove(self):
+        """Remove the container directories"""
+        # for some reason, I can't remove in-process?
+        subprocess.call(['/bin/rm', '-rf', str(self.container_dir)])
+
+
+class _ForkBarrier:
+    def __init__(self):
+        self.is_parent = True
+        self.is_child = False
+        self.other_pid = None
+        self._evt = threading.Event()
+
+    def __enter__(self):
+        signal.signal(signal.SIGUSR1, lambda sig, frame: self._evt.set())
+        parent_pid = os.getpid()
+        pid = os.fork()
+        self.other_pid = pid or parent_pid
+
+        self.is_child = pid == 0
+        self.is_parent = not self.is_child
+
+        return self
+
+    def __exit__(self, etype, evalue, traceback):
+        signal.signal(signal.SIGUSR1, signal.SIG_DFL)
+
+    def signal(self):
+        """Signal the other side"""
+        os.kill(self.other_pid, signal.SIGUSR1)
+
+    def wait(self):
+        """Wait for a signal called from the other side"""
+        self._evt.wait()
+        self._evt.clear()
+
+
+def _find_ids(source_path) -> list[str]:
+    ids = []
+    try:
+        with (source_path).open() as source_file:
+            for line in source_file:
+                parts = line.split(':')
+                ids.append(parts[2])
+    except IOError:
+        pass
+    return ids
+
+
+def _get_container_spec(container_fs: _ContainerFS, uidmapper: _UIDMapper):
+    evt = threading.Event()
+    ids_storage_path = container_fs.container_dir / 'container.json'
+    signal.signal(signal.SIGUSR1, lambda sig, frame: evt.set())
+
+    with _ForkBarrier() as barrier:
+        if barrier.is_child:
+            libc.unshare(CLONE_NEWUSER | CLONE_NEWCGROUP | CLONE_NEWNS)
+            barrier.signal()
+            barrier.wait()
+            container_fs.mount()
+
+            container = {
+                'uids': _find_ids(container_fs.container_chroot / 'etc/passwd'),
+                'gids': _find_ids(container_fs.container_chroot / 'etc/group')
+            }
+            with ids_storage_path.open('w') as container_file:
+                json.dump(container, container_file)
+
+            barrier.signal()
+            sys.exit(0)
+        else:
+            barrier.wait()
+
+            uidmapper.newuidmap(barrier.other_pid, [])
+            uidmapper.newgidmap(barrier.other_pid, [])
+
+            barrier.signal()
+            barrier.wait()
+
+            with ids_storage_path.open() as container_file:
+                container_data = json.load(container_file)
+
+            return container_data['uids'], container_data['gids']
+
+def _choose_image(storage: Storage, image_name: str) -> Image:
+    if image_name.startswith('docker.io/'):
+        image_name = image_name.replace('docker.io/', '', 1)
+        return load_docker_image(storage, image_name)
+    raise ValueError(f'unknown image reference: {image_name}')
+
+def run_command(
+    storage: Storage,
+    image_name: str,
+    container_name: str | None=None,
+    daemon: bool=False,
+    volumes: list[str] | None=None,
+):
+    """The CLI command that runs a container"""
+    if not container_name:
+        container_name = str(uuid.uuid4())
+    img = _choose_image(storage, image_name)
+
+    uidmapper = _UIDMapper('100000', '100000', '1000')
+    container = Container(
+        container_id=container_name,
+        image_registry=img.registry,
+        image_name=img.manifest.name,
+        pid=None,
+    )
+    storage.store_container(container)
+
+    container_fs = _ContainerFS.prepare(
+        storage.make_container_dir(container_name, img),
+        [ll.path for ll in img.layers],
+    )
+    container_fs.add_volumes(volumes)
+
+    entrypoint = img.config.config['Entrypoint']
+    command = img.config.config['Cmd']
 
     if not entrypoint:
         entrypoint = [command[0]]
-    parentpid = os.getpid()
 
-    upper = container_upperdir
-    workdir = container_workdir
-    mnt_cmd = [
-        '/bin/mount', '-t', 'overlay', 'overlay',
-        '-o', f'lowerdir={lowers},upperdir={upper},workdir={workdir}',
-        f'{container_chroot}'
-    ]
+    container_uids, container_gids = _get_container_spec(
+        container_fs, uidmapper)
 
-    newuidmap_args, newgidmap_args = _get_container_spec(mnt_cmd, container_dir)
+    with _ForkBarrier() as barrier:
+        if barrier.is_child:
+            libc.unshare(CLONE_NEWUSER | CLONE_NEWCGROUP | CLONE_NEWNS)
+            barrier.signal()
+            barrier.wait()
 
-    evt = threading.Event()
-    signal.signal(signal.SIGUSR1, lambda sig, frame: evt.set())
+            container_fs.mount()
+            container_fs.chroot()
+            os.execv(entrypoint[0], entrypoint + command)
 
-    forkpid = os.fork()
-    if forkpid == 0:
-        libc.unshare(CLONE_NEWUSER | CLONE_NEWCGROUP | CLONE_NEWNS)
-        os.kill(parentpid, signal.SIGUSR1)
+        else:
+            barrier.wait()
+            container.pid = barrier.other_pid
+            storage.store_container(container)
 
-        evt.wait()
-        evt.clear()
-        subprocess.call(mnt_cmd)
+            uidmapper.newuidmap(barrier.other_pid, container_uids)
+            uidmapper.newgidmap(barrier.other_pid, container_gids)
+            barrier.signal()
 
-        os.chroot(container_chroot)
-        os.execv(entrypoint[0], entrypoint + command)
+            if not daemon:
+                signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-    else:
-        evt.wait()
-        evt.clear()
-        container.pid = forkpid
-        storage.store_container(container)
-
-        subprocess.call(['/usr/bin/newuidmap', str(forkpid)] + newuidmap_args)
-        subprocess.call(['/usr/bin/newgidmap', str(forkpid)] + newgidmap_args)
-
-        os.kill(forkpid, signal.SIGUSR1)
-        signal.signal(signal.SIGUSR1, signal.SIG_DFL)
-
-        if not daemon:
-            signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-            _, retcode = os.waitpid(forkpid, 0)
-            storage.remove_container(container)
-            # for some reason, I can't remove in-process?
-            subprocess.call(['/bin/rm', '-rf', str(container_dir)])
-            sys.exit(retcode)
+                _, retcode = os.waitpid(barrier.other_pid, 0)
+                storage.remove_container(container)
+                container_fs.remove()
+                sys.exit(retcode)

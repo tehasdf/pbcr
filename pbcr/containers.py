31c4e2b254b7999758792a02cbacf9a30e8fa8f1
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2024-05-30 02:10:52 +0200

    rm: actually kill the process when rm'ing with force
    Forgot to implement this before, heh.
    When rm -f, try to stop the process, first gently with sigterm,
    then KILL DASH NINE

--- pbcr/containers.py
+++ pbcr/containers.py
@@ -2,6 +2,8 @@
 """
 
 import os
+import signal
+import time
 
 from pbcr.types import ContainerStorage
 
@@ -11,6 +13,7 @@ def ps(storage: ContainerStorage):
     containers = storage.list_containers()
     for container in containers:
         print(container)
+
 
 def rm_container(
     storage: ContainerStorage,
@@ -22,17 +25,32 @@ def rm_container(
     if not container:
         print(f"Container {container_id} not found")
         return
+
     if container.pid:
         try:
             os.kill(container.pid, 0)
         except OSError:
-            is_running = False
+            pass
         else:
-            is_running = True
-        if is_running and not force:
-            print(
-                f"Container {container_id} is running. "
-                "Stop it first or use --force"
-            )
-            return
+            if not force:
+                print(
+                    f"Container {container_id} is running. "
+                    "Stop it first or use --force"
+                )
+                return
+            _stop_container_process(container.pid)
     storage.remove_container(container)
+
+
+def _stop_container_process(pid: int):
+    os.kill(pid, signal.SIGTERM)
+    for _ in range(50):
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            return
+        else:
+            time.sleep(0.1)
+    # after 50 retries, ie. 5 seconds, if it's still up....
+    # KILL DASH NINE
+    os.kill(pid, 9)

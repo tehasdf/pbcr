1866573354436105e28d7010d5aedd99248a9ee4
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2025-06-26 23:56:59 +0200

    Refactor processfd.py
    Use separate functions for better readability

--- pbcr/networking/processfd.py
+++ pbcr/networking/processfd.py
@@ -1,5 +1,6 @@
-"""Utilities for getting a process' network FD"""
+"""Utilities for getting a process' network file descriptors."""
 
+from dataclasses import dataclass
 import array
 import fcntl
 import socket
@@ -10,91 +11,136 @@ from pbcr import libc
 from pbcr.forkbarrier import ForkBarrier
 
 
-def get_process_net_fd(pid) -> int:
-    """Get the network FD for a process"""
-    # at some point we'll need to refactor this function
-    # pylint: disable=too-many-locals
+@dataclass
+class NetworkConfig:
+    """Configuration for network interfaces"""
+    tun_device: bytes = b'tap0'
+    tun_ip: str = '192.168.64.1'
+    tun_netmask: str = '255.255.255.0'
+    tun_flags: int = libc.IFF_TUN | libc.IFF_NO_PI
+    up_flags: int = libc.IFF_UP | libc.IFF_RUNNING
+
+def _setup_network_namespaces(pid: int) -> None:
+    """Enter the process's user and network namespaces.
+
+    Args:
+        pid: Process ID to enter namespaces of
+    """
+    with open(f'/proc/{pid}/ns/user', 'rb') as userns_file, \
+            open(f'/proc/{pid}/ns/net', 'rb') as netns_file:
+        libc.setns(int(userns_file.fileno()), libc.CLONE_NEWUSER)
+        libc.setns(int(netns_file.fileno()), libc.CLONE_NEWNET)
+
+
+def _create_tun_device(config: NetworkConfig) -> int:
+    """Create and configure a TUN device.
+
+    Args:
+        config: Network configuration to use
+
+    Returns:
+        File descriptor of the created TUN device
+    """
+    ifreq = struct.pack(
+        f"{libc.IFNAMSIZ}sH", 
+        config.tun_device,
+        config.tun_flags
+    )
+    # ignore pylint here, because we'll be returning the fd later
+    tun_fd = open("/dev/net/tun", "r+b", buffering=0)  # pylint: disable=consider-using-with
+    fcntl.ioctl(tun_fd, libc.TUNSETIFF, ifreq)
+    return tun_fd
+
+def _configure_network_interfaces(config: NetworkConfig) -> None:
+    """Configure loopback and TAP interfaces.
+
+    Args:
+        config: Network configuration to apply
+    """
+    lo_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock_fd = lo_sock.fileno()
+
+    # Configure loopback interface
+    fcntl.ioctl(
+        sock_fd,
+        libc.SIOCSIFFLAGS,
+        struct.pack(f"{libc.IFNAMSIZ}sH", b'lo', config.up_flags)
+    )
+    # Configure TUN interface
+    fcntl.ioctl(
+        sock_fd,
+        libc.SIOCSIFFLAGS,
+        struct.pack(f"{libc.IFNAMSIZ}sH", config.tun_device, config.up_flags)
+    )
+    # Set IP address
+    ip_bytes = socket.inet_aton(config.tun_ip)
+    fcntl.ioctl(
+        sock_fd,
+        libc.SIOCSIFADDR,
+        struct.pack('16sI14s', config.tun_device, socket.AF_INET, ip_bytes)
+    )
+
+    # Set netmask
+    nm_bytes = socket.inet_aton(config.tun_netmask)
+    fcntl.ioctl(
+        sock_fd,
+        libc.SIOCSIFNETMASK,
+        struct.pack('16sI14s', config.tun_device, socket.AF_INET, nm_bytes)
+    )
+
+
+def _child_process_operations(
+    pid: int,
+    left_sock: socket.socket,
+    barrier: ForkBarrier,
+    config: NetworkConfig
+) -> None:
+    """Operations performed in the child process.
+
+    Args:
+        pid: Process ID to configure
+        left_sock: Socket for communication
+        barrier: Synchronization barrier
+        config: Network configuration to apply
+    """
+    _setup_network_namespaces(pid)
+    tun_fd = _create_tun_device(config)
+    _configure_network_interfaces(config)
+
+    barrier.signal()
+    left_sock.sendmsg(
+        [b'ok'],
+        [(socket.SOL_SOCKET, socket.SCM_RIGHTS, struct.pack('i', tun_fd.fileno()))]
+    )
+    barrier.wait()
+    sys.exit(0)
+
+def get_process_net_fd(pid: int, config: NetworkConfig = None) -> int:
+    """Get the network file descriptor for a process.
+
+    Args:
+        pid: Process ID to get FD for
+        config: Optional network configuration
+
+    Returns:
+        Network file descriptor
+    """
+    if config is None:
+        config = NetworkConfig()
     left_sock, right_sock = socket.socketpair()
+
     with ForkBarrier() as barrier:
         if barrier.is_child:
-            with open(f'/proc/{pid}/ns/user', 'rb') as userns_file, \
-                    open(f'/proc/{pid}/ns/net', 'rb') as netns_file:
-
-                libc.setns(
-                    int(userns_file.fileno()),
-                    libc.CLONE_NEWUSER,
-                )
-                libc.setns(
-                    int(netns_file.fileno()),
-                    libc.CLONE_NEWNET,
-                )
-
-            dev_name = b'tap0'
-            ifreq = struct.pack(
-                f"{libc.IFNAMSIZ}sH",
-                dev_name,
-                libc.IFF_TUN | libc.IFF_NO_PI,
-            )
-            tun_fd = open("/dev/net/tun", "r+b", buffering=0)  # pylint: disable=consider-using-with
-            fcntl.ioctl(tun_fd, libc.TUNSETIFF, ifreq)
-
-            lo_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock_fd = lo_sock.fileno()
-            lo_ifreq = struct.pack(
-                f"{libc.IFNAMSIZ}sH",
-                b'lo',
-                libc.IFF_UP | libc.IFF_RUNNING,
-            )
-            fcntl.ioctl(sock_fd, libc.SIOCSIFFLAGS, lo_ifreq)
-
-            fcntl.ioctl(
-                sock_fd,
-                libc.SIOCSIFFLAGS,
-                struct.pack(
-                    f"{libc.IFNAMSIZ}sH",
-                    dev_name,
-                    libc.IFF_UP | libc.IFF_RUNNING,
-                )
-            )
-            ip_text = '192.168.64.1'
-            ip_bytes = socket.inet_aton(ip_text)
-            fcntl.ioctl(
-                sock_fd,
-                libc.SIOCSIFADDR,
-                struct.pack('16sI14s', b'tap0', socket.AF_INET, ip_bytes)
-            )
-            nm_text = '255.255.255.0'
-            nm_bytes = socket.inet_aton(nm_text)
-            fcntl.ioctl(
-                sock_fd,
-                libc.SIOCSIFNETMASK,
-                struct.pack('16sI14s', b'tap0', socket.AF_INET, nm_bytes),
-            )
-            barrier.signal()
-            left_sock.sendmsg(
-                [b'ok'],
-                [
-                    (
-                        socket.SOL_SOCKET,
-                        socket.SCM_RIGHTS,
-                        struct.pack('i', tun_fd.fileno())
-                    )
-                ],
-            )
-            barrier.wait()
-            sys.exit(0)
+            _child_process_operations(pid, left_sock, barrier, config)
 
         barrier.wait()
         fds = array.array("i")
         parts = right_sock.recvmsg(4096, socket.CMSG_LEN(1 * fds.itemsize))
-        ancdata = parts[1]
-        for cmsg_level, cmsg_type, cmsg_data in ancdata:
-            if (
-                cmsg_level == socket.SOL_SOCKET and
-                cmsg_type == socket.SCM_RIGHTS
-            ):
-                # Append data, ignoring any truncated integers at the end.
+
+        for cmsg_level, cmsg_type, cmsg_data in parts[1]:
+            if cmsg_level == socket.SOL_SOCKET and cmsg_type == socket.SCM_RIGHTS:
                 data_end = len(cmsg_data) - (len(cmsg_data) % fds.itemsize)
                 fds.frombytes(cmsg_data[:data_end])
+
         barrier.signal()
         return fds[0]

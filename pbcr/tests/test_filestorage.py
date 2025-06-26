a7e437e85edfee28d20c995f024fb92c71f15561
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2025-06-26 23:07:03 +0200

    Fix filestorage unittests
    This fixes the unittests in test_filestorage.
    This is because pytest's tmpdir is a LocalPath and is not exactly
    compatible with pathlib's Path. So we can't use Path methods on it, we
    need to make it into a Path :)

--- pbcr/tests/test_filestorage.py
+++ pbcr/tests/test_filestorage.py
@@ -1,17 +1,19 @@
 """File storage tests"""
+from pathlib import Path
+
 from pbcr.storage import FileImageStorage
 
 
 def test_make_filestorage(tmpdir):
     """make_storage creates a FileStorage with an existing dir structure"""
-    target = tmpdir / 'pbcr'
+    target = Path(tmpdir) / 'pbcr'
     assert not target.exists()
     storage = FileImageStorage.create(target)
     assert isinstance(storage, FileImageStorage)
-    assert target.isdir()
+    assert target.is_dir()
 
 
 def test_no_images(tmpdir):
     """An empty storage returns no images"""
-    storage = FileImageStorage(tmpdir)
+    storage = FileImageStorage(Path(tmpdir))
     assert not storage.list_images()

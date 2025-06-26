3ca513468cdbddc23ceb455c9bdb324c76530dc7
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2025-06-26 23:11:23 +0200

    Use tmp_path instead

--- pbcr/tests/test_filestorage.py
+++ pbcr/tests/test_filestorage.py
@@ -4,16 +4,16 @@ from pathlib import Path
 from pbcr.storage import FileImageStorage
 
 
-def test_make_filestorage(tmpdir):
+def test_make_filestorage(tmp_path):
     """make_storage creates a FileStorage with an existing dir structure"""
-    target = Path(tmpdir) / 'pbcr'
+    target = tmp_path / 'pbcr'
     assert not target.exists()
     storage = FileImageStorage.create(target)
     assert isinstance(storage, FileImageStorage)
     assert target.is_dir()
 
 
-def test_no_images(tmpdir):
+def test_no_images(tmp_path):
     """An empty storage returns no images"""
-    storage = FileImageStorage(Path(tmpdir))
+    storage = FileImageStorage(tmp_path / 'pbcr')
     assert not storage.list_images()
